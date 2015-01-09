f59ba24a969ce8da9047289720f1785ebd6f8d7b
Justin <jsousa1919@gmail.com>
2015-01-08 23:36:45 -0500

    Got images working, now just for state management and rotation

--- froggy.py
+++ froggy.py
@@ -5,8 +5,9 @@ import random
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.graphics import Color, Rectangle
-from kivy.properties import ObjectProperty
+from kivy.properties import NumericProperty, ObjectProperty, ListProperty
 from kivy.uix.button import Button
+from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
@@ -28,19 +29,32 @@ class MainScreen(FloatLayout):
     def start_game(self):
         self.parent.start_game()
 
+class Graphic(Image):
+    state = 0
+    angle = 0
+    @classmethod
+    def get(cls, name):
+        return 'assets/images/{}'.format(name)
+
+class Frog(Graphic):
+    sources = [
+        Graphic.get('froggy1.png'),
+        Graphic.get('froggyleft.png'),
+        Graphic.get('froggyright.png')
+    ]
+
 class FroggyGame(FloatLayout):
     frog = ObjectProperty(None)
     target = None
     hopping = False
-    speed = 10
+    speed = 5
 
     def frog_pos(self):
         return np.array((self.frog.center_x, self.frog.center_y))
 
-    def inv_proximity(self, pos):
-        return np.abs(
-            (np.linalg.norm(pos - self.target) - (self.distance / 2)) / (self.distance / 2)
-        )
+    def remoteness(self):
+        raw = np.abs(np.linalg.norm(self.frog_pos() - self.halfway))
+        return 1 - (2 * raw / self.distance)
 
     def frog_move(self):
         current = self.frog_pos()
@@ -51,7 +65,8 @@ class FroggyGame(FloatLayout):
             self.frog_stop()
         else:
             new = (current + ((vector / mag) * self.speed))
-            self.frog.font_size = 64 + int((1 - self.inv_proximity(new)) * 128)
+            self.frog.font_size = 64 + int(self.remoteness() * 128)
+        import ipdb; ipdb.set_trace() 
         self.frog.center_x, self.frog.center_y = map(float, new)
 
     def frog_stop(self):
@@ -66,8 +81,10 @@ class FroggyGame(FloatLayout):
             self.origin = self.frog_pos()
             self.target = np.array(touch.pos)
             self.vector = self.target - self.origin
+            self.halfway = self.origin + (self.vector / 2)
             self.distance = np.linalg.norm(self.vector)
-            self.frog.color = [random.random(), random.random(), random.random(), 1]
+            self.unit = self.vector / self.distance
+            #self.frog.color = [random.random(), random.random(), random.random(), 1]
 
     def update(self, dt):
         if self.target is not None:
